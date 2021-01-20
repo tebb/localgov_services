6aebe346f409a63c125b006f289f0daef9144b36
Matt Fletcher <matt.fletcher2011@gmail.com>
2021-01-20 15:53:25 +0000

    initial push of services menu group testing

---
+++ tests/src/FunctionalJavascript/ServicesMenuGroupTest.php
@@ -0,0 +1,71 @@
+<?php
+
+namespace Drupal\Tests\localgov_services\FunctionalJavascript;
+
+use Drupal\FunctionalJavascriptTests\WebDriverTestBase;
+use Drupal\node\NodeInterface;
+
+/**
+ * Javascript tests for LocalGovDrupal services menu link group.
+ */
+class ServicesMenuGroupTest extends WebDriverTestBase {
+
+/**
+   * {@inheritdoc}
+   */
+  protected $defaultTheme = 'localgov_theme';
+
+  /**
+   * {@inheritdoc}
+   */
+  protected $profile = 'localgov';
+
+  /**
+   * Modules to enable.
+   *
+   * @var array
+   */
+  public static $modules = [
+    'localgov_services',
+    'localgov_services_landing',
+    'localgov_services_navigation',
+    'localgov_services_page',
+    'localgov_services_status',
+    'localgov_services_sublanding',
+    'localgov_menu_link_group',
+  ];
+
+public function testServicesMenuLinkGroup() {
+
+  $parameters = new MenuTreeParameters();
+  $parameters->onlyEnabledLinks();
+
+  /** @var \Drupal\Core\Menu\MenuLinkTreeElement[] $main_menu_tree */
+  $admin_menu = \Drupal::service('menu.link_tree')->load('add', $parameters);
+
+  $this->assertCount(9, $admin_menu, 'Admin menu only has 9 top level items.');
+  $this->assertArrayHasKey('my.route1', $admin_menu);
+  $this->assertArrayHasKey('my.route2', $admin_menu);
+  $this->assertArrayHasKey('my.route3', $admin_menu);
+  $this->assertArrayHasKey('my.route4', $admin_menu);
+  $this->assertArrayHasKey('my.route5', $admin_menu);
+  $this->assertArrayHasKey('my.route6', $admin_menu);
+  $this->assertArrayHasKey('my.route7', $admin_menu);
+  $this->assertArrayHasKey('my.route8', $admin_menu);
+  $this->assertArrayHasKey('my.route9', $admin_menu);
+
+  // Check the Route2 menu subtree.
+  $this->assertTrue($admin_menu['my.route2']->hasChildren, 'Route2 menu has children');
+  /** @var \Drupal\Core\Menu\[] $route2_menu */
+  $route2_menu = $admin_menu['my.route2']->subtree;
+
+  $this->assertCount(2, $route1_menu, 'Route1 menu has exactly 2 children');
+  $this->assertArrayHasKey('my.submroute1', $route1_menu);
+  $this->assertArrayHasKey('my.submroute2', $route1_menu);
+
+  // Check an item without children.
+  $this->assertFalse($main_menu_tree['my.route2']->hasChildren, 'Route2 menu has no children');
+
+}
+
+}
